fe840f4fb6fd8952f918a7bbbb18c93736952aae
Rishi Ghan <hghan@apple.com>
2019-10-18 18:48:04 -0700

    Added utils for gAPI auth

---
+++ utils/googleapi.js
@@ -0,0 +1,22 @@
+const { google }  = require("googleapis");
+const _ = require("lodash");
+const dotenv = require("dotenv");
+const gAPICredentials = dotenv.config();
+
+const jwtClient = new google.auth.JWT(gAPICredentials.parsed.CLIENT_EMAIL,
+	null,
+	gAPICredentials.parsed.PRIVATE_KEY,
+	"https://www.googleapis.com/auth/analytics.readonly");
+
+const analyticsQuery = async (queryString)  => {
+	const parsedQueryJSON = JSON.parse(queryString);
+	const authResponse = await jwtClient.authorize();
+	const queryConfig = _.extend({"auth": jwtClient}, parsedQueryJSON);
+	const analyticsData = await google.analytics("v3").data.ga.get(queryConfig);
+	return analyticsData;
+};
+
+module.exports = {
+	jwtClient,
+	analyticsQuery
+};
