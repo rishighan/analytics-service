b9a199990b707dcc5879c5a8733d6eb6eb921cc5
Rishi Ghan <hghan@apple.com>
2019-11-07 15:02:10 -0800

    Added a redis port config check

--- config/redis.config.js
+++ config/redis.config.js
@@ -1,6 +1,6 @@
 // redis connection config
 let redis = require("redis");
-const REDIS_PORT = "6379";
+const REDIS_PORT = process.env.REDIS_PORT ? process.env.REDIS_PORT : "6379";
 const REDIS_HOST = process.env.REDIS_HOST ? process.env.REDIS_HOST : "localhost";
 let client = redis.createClient(REDIS_PORT, REDIS_HOST);
 
