623279094b6a5bf68055eb500aea0d621dbeab2d
Rishi Ghan <rishi.ghan@gmail.com>
2019-11-21 08:21:32 -0800

    Added node-zookeeper-client

--- utils/googleapi.js
+++ utils/googleapi.js
@@ -1,40 +1,56 @@
 const { google } = require("googleapis");
 const _ = require("lodash");
 const redis = require("../config/redis.config");
-const ZooKeeper = require("zookeeper").Promise;
+const zookeeper = require("node-zookeeper-client");
 
-function createClient(timeoutMs = 5000) {
-	const config = {
-		connect: process.env.ZOOKEEPER_HOST,
-		timeout: timeoutMs,
-		debug_level: ZooKeeper.ZOO_LOG_LEVEL_WARN,
-		host_order_deterministic: false,
-	};
+const client = zookeeper.createClient(process.env.ZOOKEEPER_HOST);
+const path = process.argv[2];
 
-	return new ZooKeeper(config);
-}
+listChildren = (client, path) => {
+  client.getChildren(
+    path,
+    event => {
+      console.log("Got watcher event: %s", event);
+      listChildren(client, path);
+    },
+    (error, children, stat) => {
+      if (error) {
+        console.log("Failed to list children of %s due to: %s.", path, error);
+        return;
+      }
 
-const client = createClient();
-console.log("ZOOKEEPER HOST-> ", client);
+      console.log("Children of %s are: %j.", path, children);
+    }
+  );
+};
+
+client.once("connected", function() {
+  console.log("Connected to ZooKeeper.");
+  listChildren(client, path);
+});
+
+client.connect();
 
 let jwtClient;
 redis.client.hgetall("googleapi", (error, data) => {
-	const privateKey = _.replace(data.private_key, /\\n/g, "\n");	
-	jwtClient = new google.auth.JWT(data.client_email,
-		null,
-		privateKey,
-		"https://www.googleapis.com/auth/analytics.readonly");
+  const privateKey = _.replace(data.private_key, /\\n/g, "\n");
+  jwtClient = new google.auth.JWT(
+    data.client_email,
+    null,
+    privateKey,
+    "https://www.googleapis.com/auth/analytics.readonly"
+  );
 });
 
-const analyticsQuery = async (queryString) => {
-	const parsedQueryJSON = JSON.parse(queryString);
-	const authResponse = await jwtClient.authorize();
-	const queryConfig = _.extend({ "auth": jwtClient }, parsedQueryJSON);
-	const analyticsData = await google.analytics("v3").data.ga.get(queryConfig);
-	return analyticsData;
+const analyticsQuery = async queryString => {
+  const parsedQueryJSON = JSON.parse(queryString);
+  const authResponse = await jwtClient.authorize();
+  const queryConfig = _.extend({ auth: jwtClient }, parsedQueryJSON);
+  const analyticsData = await google.analytics("v3").data.ga.get(queryConfig);
+  return analyticsData;
 };
 
 module.exports = {
-	jwtClient,
-	analyticsQuery
+  jwtClient,
+  analyticsQuery
 };
